bf119531701232d34a6b3cc179205a278e24dbee
Thomas Wilson <tommyhwilson3@gmail.com>
2023-05-04 15:51:40 -0500

    reverted changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import FileForm from "./FileForm";
 
 const BASE_URL = "http://localhost:5001";
 
 function Home() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isFilePicked, setIsFilePicked] = useState(false);
   const [images, setImages] = useState([]);
 
-  // const changeHandler = (evt) => {
-  //   setSelectedFile(evt.target.files[0]);
-  //   setIsFilePicked(true);
-  // };
+  const changeHandler = (evt) => {
+    setSelectedFile(evt.target.files[0]);
+    setIsFilePicked(true);
+  };
 
-  async function addImage(evt) {
+  async function handleSubmit(evt) {
     evt.preventDefault();
     const formData = new FormData();
     formData.append("image", selectedFile);
@@ -37,30 +38,26 @@ function Home() {
   }
 
   return (
-    <FileForm handleSubmit={addImage}/>
-
-  )
-  // return (
-  //   <div>
-  //     <input type="file" name="file" onChange={changeHandler} />
-  //     {isFilePicked ? (
-  //       <div>
-  //         <p>Filename: {selectedFile.name}</p>
-  //         <p>Filetype: {selectedFile.type}</p>
-  //         <p>Size in bytes: {selectedFile.size}</p>
-  //         <p>
-  //           lastModifiedDate:{" "}
-  //           {selectedFile.lastModifiedDate.toLocaleDateString()}
-  //         </p>
-  //       </div>
-  //     ) : (
-  //       <p>Select a file to show details</p>
-  //     )}
-  //     <div>
-  //       <button onClick={handleSubmit}>Submit</button>
-  //     </div>
-  //   </div>
-  // );
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {isFilePicked ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+          <p>
+            lastModifiedDate:{" "}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
